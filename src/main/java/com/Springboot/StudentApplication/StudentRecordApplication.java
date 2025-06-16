9fa6d5e053419c8427d2dc9fe6161bd71728ad65
Harshil6409 <hrshil6409@gmail.com>
2025-06-16 15:56:46 +0530

    Feature-1 changes made as per comments

--- src/main/java/com/Springboot/StudentApplication/StudentRecordApplication.java
+++ src/main/java/com/Springboot/StudentApplication/StudentRecordApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StudentRecordApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(StudentRecordApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StudentRecordApplication.class, args);
+    }
 }

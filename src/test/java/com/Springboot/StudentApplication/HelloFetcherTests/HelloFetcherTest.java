c19780680b4b5c8bf0ac5b754ee5fdf0b89c70f0
Harshil6409 <hrshil6409@gmail.com>
2025-06-17 08:48:26 +0530

    Feature-1 changes made as per comments

--- src/test/java/com/Springboot/StudentApplication/HelloFetcherTests/HelloFetcherTest.java
+++ src/test/java/com/Springboot/StudentApplication/HelloFetcherTests/HelloFetcherTest.java
@@ -3,12 +3,26 @@ package com.Springboot.StudentApplication.HelloFetcherTests;
 import com.Springboot.StudentApplication.HelloFetcher.HelloFetcher;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class HelloFetcherTest {
     @Test
     public void helloTest() {
         HelloFetcher helloFetcher = new HelloFetcher();
         String hello = helloFetcher.hello();
-        System.out.println(hello.equals("Hello Intern!"));
+        assertEquals("Hello Intern!", hello);
+    }
+    @Test
+    public void helloNotNullTest() {
+        HelloFetcher helloFetcher = new HelloFetcher();
+        String hello = helloFetcher.hello();
+        assertNotNull(hello);
+    }
+    @Test
+    public void helloFailTest() {
+        HelloFetcher helloFetcher = new HelloFetcher();
+        String hello = helloFetcher.hello();
+        assertNotEquals("Hello Intern", hello);
     }
 }
